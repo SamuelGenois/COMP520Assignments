f9205793661cf17feeb913f26559ec01a3758655
Samuel Genois <samuel.genois@gmail.com>
2020-01-25 00:13:18 -0500

    second valid program

--- samuel_genois/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Use case: build.sh, use from root folder
-cd src
-make
-make clean
--- samuel_genois/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Use case: run.sh scan|token|parse filename in the root folder
-cat $2 | ./minlang $1
