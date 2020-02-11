c47718baf31384811747479555ff863b4f35c666
SamuelGenois <samuel.genois@mail.mcgill.ca>
2020-02-11 05:27:29 -0500

    pp fix 10

--- src/printer.c
+++ src/printer.c
@@ -12,44 +12,44 @@ void prettySTMT(STMT *stmt) {
     if(stmt == 0)
         return;
     
-    prettySTMT(stmt->next);
+    prettySTMT(stmt->val.next);
 
     switch(stmt->kind) {
         case k_exp:
-            prettyEXP(stmt->exp);
+            prettyEXP(stmt->val.exp);
             printf(";\n");
             break;
         case k_if:
             printf("if ( ");
-            prettyEXP(stmt->ifWhile.condition);
+            prettyEXP(stmt->val.ifWhile.condition);
             printf(" )\n");
-            prettySTMT(stmt->ifWhile.block);
+            prettySTMT(stmt->val.ifWhile.block);
             break;
         case k_ifelse:
             printf("if ( ");
-            prettyEXP(stmt->ifElse.condition);
+            prettyEXP(stmt->val.ifElse.condition);
             printf(" )\n");
-            prettySTMT(stmt->ifElse.block1);
+            prettySTMT(stmt->val.ifElse.block1);
             printf("else\n");
-            prettySTMT(stmt->ifElse.block2);
+            prettySTMT(stmt->val.ifElse.block2);
             break;
         case k_while:
             printf("while ( ");
-            prettyEXP(stmt->ifWhile.condition);
+            prettyEXP(stmt->val.ifWhile.condition);
             printf(" )\n");
-            prettySTMT(stmt->ifWhile.block);
+            prettySTMT(stmt->val.ifWhile.block);
             break;
         case k_read:
-            printf("read(%s);\n", stmt->readIdentifier);
+            printf("read(%s);\n", stmt->val.readIdentifier);
             break;
         case k_print:
             printf("print(");
-            prettyEXP(stmt->exp);
+            prettyEXP(stmt->val.exp);
             printf(")\n");
             break;
         case k_block:
             printf("{\n");
-            prettySTMT(stmt->blockContent);
+            prettySTMT(stmt->val.blockContent);
             printf("\n}\n");
             break;
     }
@@ -59,136 +59,136 @@ void prettyEXP(EXP *exp)
 {
     switch (exp->kind) {
         case k_Decl:
-            switch(exp->declaration) {
+            switch(exp->val.declaration) {
                 case type_int:
-                    printf("var %s : int = ", exp->declaration.identifier)
+                    printf("var %s : int = ", exp->val.declaration.identifier)
                     break;
                 case type_bool:
-                    printf("var %s : bool = ", exp->declaration.identifier)
+                    printf("var %s : bool = ", exp->val.declaration.identifier)
                     break;
                 case type_float:
-                    printf("var %s : float = ", exp->declaration.identifier)
+                    printf("var %s : float = ", exp->val.declaration.identifier)
                     break;
                 case type_string:
-                    printf("var %s : string = ", exp->declaration.identifier)
+                    printf("var %s : string = ", exp->val.declaration.identifier)
                     break;
                 case type_infer:
-                    printf("var %s = ", exp->declaration.identifier)
+                    printf("var %s = ", exp->val.declaration.identifier)
                     break;
             }
-            prettyEXP(exp->declaration.exp);
+            prettyEXP(exp->val.declaration.exp);
             break;
         case k_Identifier:
-            printf("%s", exp->identifierExp.identifier);
+            printf("%s", exp->val.identifierExp.identifier);
             break;
         case k_IntLiteral:
-            printf("%i", exp->intLiteral);
+            printf("%i", exp->val.intLiteral);
             break;
         case k_FloatLiteral:
-            printf("%f", exp->floatLiteral);
+            printf("%f", exp->val.floatLiteral);
             break;
         case k_BoolLiteral:
-            if(exp->boolLiteral)
+            if(exp->val.boolLiteral)
                 printf("True");
             else
                 printf("False");
             break;
         case k_StringLiteral:
-            printf("%s", exp->strLiteral);
+            printf("%s", exp->val.strLiteral);
             break;
         case k_Addition:
             printf("( ");
-            prettyEXP(exp->binary.lhs);
+            prettyEXP(exp->val.binary.lhs);
             printf(" + ");
-            prettyEXP(exp->binary.rhs);
+            prettyEXP(exp->val.binary.rhs);
             printf(" )");
             break;
         case k_Subtraction:
             printf("( ");
-            prettyEXP(exp->binary.lhs);
+            prettyEXP(exp->val.binary.lhs);
             printf(" - ");
-            prettyEXP(exp->binary.rhs);
+            prettyEXP(exp->val.binary.rhs);
             printf(" )");
             break;
         case k_Multiplication:
             printf("( ");
-            prettyEXP(exp->binary.lhs);
+            prettyEXP(exp->val.binary.lhs);
             printf(" * ");
-            prettyEXP(exp->binary.rhs);
+            prettyEXP(exp->val.binary.rhs);
             printf(" )");
             break;
         case k_Division:
             printf("( ");
-            prettyEXP(exp->binary.lhs);
+            prettyEXP(exp->val.binary.lhs);
             printf(" / ");
-            prettyEXP(exp->binary.rhs);
+            prettyEXP(exp->val.binary.rhs);
             printf(" )");
         case k_UnaryMinus:
             printf("-( ");
-            prettyEXP(exp->exp);
+            prettyEXP(exp->val.exp);
             printf(" )");
         case k_Assignment:
-            printf("%s = ", exp->assignment.identifier);
+            printf("%s = ", exp->val.assignment.identifier);
             break;
         case k_Or:
             printf("( ");
-            prettyEXP(exp->binary.lhs);
+            prettyEXP(exp->val.binary.lhs);
             printf(" || ");
-            prettyEXP(exp->binary.rhs);
+            prettyEXP(exp->val.binary.rhs);
             printf(" )");
             break;
         case k_And:
             printf("( ");
-            prettyEXP(exp->binary.lhs);
+            prettyEXP(exp->val.binary.lhs);
             printf(" && ");
-            prettyEXP(exp->binary.rhs);
+            prettyEXP(exp->val.binary.rhs);
             printf(" )");
             break;
         case k_Not:
             printf("!( ");
-            prettyEXP(exp->exp);
+            prettyEXP(exp->val.exp);
             printf(" )");
             break;
         case k_Equal:
             printf("( ");
-            prettyEXP(exp->binary.lhs);
+            prettyEXP(exp->val.binary.lhs);
             printf(" == ");
-            prettyEXP(exp->binary.rhs);
+            prettyEXP(exp->val.binary.rhs);
             printf(" )");
             break;
         case k_NotEqual:
             printf("( ");
-            prettyEXP(exp->binary.lhs);
+            prettyEXP(exp->val.binary.lhs);
             printf(" != ");
-            prettyEXP(exp->binary.rhs);
+            prettyEXP(exp->val.binary.rhs);
             printf(" )");
             break;
         case k_Greater:
             printf("( ");
-            prettyEXP(exp->binary.lhs);
+            prettyEXP(exp->val.binary.lhs);
             printf(" > ");
-            prettyEXP(exp->binary.rhs);
+            prettyEXP(exp->val.binary.rhs);
             printf(" )");
             break;
         case k_GreaterOrEqual:
             printf("( ");
-            prettyEXP(exp->binary.lhs);
+            prettyEXP(exp->val.binary.lhs);
             printf(" >= ");
-            prettyEXP(exp->binary.rhs);
+            prettyEXP(exp->val.binary.rhs);
             printf(" )");
             break;
         case k_Less:
             printf("( ");
-            prettyEXP(exp->binary.lhs);
+            prettyEXP(exp->val.binary.lhs);
             printf(" < ");
-            prettyEXP(exp->binary.rhs);
+            prettyEXP(exp->val.binary.rhs);
             printf(" )");
             break;
         case k_LessOrEqual:
             printf("( ");
-            prettyEXP(exp->binary.lhs);
+            prettyEXP(exp->val.binary.lhs);
             printf(" <= ");
-            prettyEXP(exp->binary.rhs);
+            prettyEXP(exp->val.binary.rhs);
             printf(" )");
             break;
     }
